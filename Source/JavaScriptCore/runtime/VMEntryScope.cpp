7402b7299d08f8feec296dd89fe0d3c4b494c5a8
Yusuke Suzuki <ysuzuki@apple.com>
2022-05-31 04:13:54 +0000

    [JSC] Make VMEntryScope cheap https://bugs.webkit.org/show_bug.cgi?id=241091
    Reviewed by Mark Lam.
    
    This patch makes VMEntryScope cheap. In some microbenchmarks, we observe repeated execution of VMEntryScope
    because of many ticks driven by microtasks. And currently VMEntryScope is designed to be non-cheap based on
    the assumption that this is not frequently executed.
    
    1. We add isJSThread flag to Thread so that we can skip some of unnecessary initializations.
    2. Appropriately set UNLIKELY / LIKELY for the major path.
    3. Make DateCache::resetIfNecessary fast path inlined.
    
        ToT
        Time(doxbee-async-es2017-native): 24 ms.
        Time(doxbee-promises-es2015-native): 44.9 ms.
        Time(fibonacci-async-es2017-native): 197.6 ms.
        Time(parallel-async-es2017-native): 109.1 ms.
        Time(parallel-promises-es2015-native): 80.4 ms.
    
        Patched
        Time(doxbee-async-es2017-native): 21.4 ms.
        Time(doxbee-promises-es2015-native): 36.4 ms.
        Time(fibonacci-async-es2017-native): 168.1 ms.
        Time(parallel-async-es2017-native): 103.7 ms.
        Time(parallel-promises-es2015-native): 70.9 ms.
    
    * Source/JavaScriptCore/runtime/JSDateMath.cpp:
    (JSC::DateCache::resetIfNecessarySlow):
    (JSC::DateCache::resetIfNecessary): Deleted.
    * Source/JavaScriptCore/runtime/JSDateMath.h:
    (JSC::DateCache::resetIfNecessary):
    * Source/JavaScriptCore/runtime/VM.h:
    (JSC::VM::firePrimitiveGigacageEnabledIfNecessary):
    * Source/JavaScriptCore/runtime/VMEntryScope.cpp:
    (JSC::VMEntryScope::VMEntryScope):
    (JSC::VMEntryScope::~VMEntryScope):
    * Source/WTF/wtf/Threading.cpp:
    (WTF::Thread::registerJSThread):
    * Source/WTF/wtf/Threading.h:
    
    Canonical link: https://commits.webkit.org/251135@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295040 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/VMEntryScope.cpp
+++ Source/JavaScriptCore/runtime/VMEntryScope.cpp
@@ -44,18 +44,19 @@ VMEntryScope::VMEntryScope(VM& vm, JSGlobalObject* globalObject)
     if (!vm.entryScope) {
         vm.entryScope = this;
 
-#if ENABLE(SINGLE_THREADED_VM_ENTRY_SCOPE)
-        if (vm.entryScopeID != reinterpret_cast<void*>(globalObject)) {
-            vm.entryScopeID = globalObject;
-#endif
+        auto& thread = Thread::current();
+        if (UNLIKELY(!thread.isJSThread())) {
+            Thread::registerJSThread(thread);
+
 #if ENABLE(WEBASSEMBLY)
-        if (Wasm::isSupported())
-            Wasm::startTrackingCurrentThread();
+                if (Wasm::isSupported())
+                    Wasm::startTrackingCurrentThread();
 #endif
 
 #if HAVE(MACH_EXCEPTIONS)
-        registerThreadForMachExceptionHandling(Thread::current());
+                registerThreadForMachExceptionHandling(thread);
 #endif
+        }
 
         vm.firePrimitiveGigacageEnabledIfNecessary();
 
@@ -63,7 +64,7 @@ VMEntryScope::VMEntryScope(VM& vm, JSGlobalObject* globalObject)
         // observe time zone changes.
         vm.resetDateCacheIfNecessary();
 
-        if (vm.watchdog())
+        if (UNLIKELY(vm.watchdog()))
             vm.watchdog()->enteredVM();
 
 #if ENABLE(SAMPLING_PROFILER)
@@ -95,10 +96,10 @@ VMEntryScope::~VMEntryScope()
 
     ASSERT_WITH_MESSAGE(!m_vm.hasCheckpointOSRSideState(), "Exitting the VM but pending checkpoint side state still available");
 
-    if (Options::useTracePoints())
+    if (UNLIKELY(Options::useTracePoints()))
         tracePoint(VMEntryScopeEnd);
     
-    if (m_vm.watchdog())
+    if (UNLIKELY(m_vm.watchdog()))
         m_vm.watchdog()->exitedVM();
 
     m_vm.entryScope = nullptr;
