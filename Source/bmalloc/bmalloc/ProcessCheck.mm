fa33b8cff43f5445be9df0040f1773611bd8c8f2
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-09-17 18:16:29 -0700

    Non-Apple JSCOnly build fixes

--- Source/bmalloc/bmalloc/ProcessCheck.mm
+++ Source/bmalloc/bmalloc/ProcessCheck.mm
@@ -54,6 +54,7 @@ bool gigacageEnabledForProcess()
 }
 #endif // !BPLATFORM(WATCHOS)
 
+#if BPLATFORM(COCOA)
 bool shouldAllowMiniMode()
 {
     // Mini mode is mainly meant for constraining memory usage in bursty daemons that use JavaScriptCore.
@@ -66,6 +67,7 @@ bool shouldAllowMiniMode()
     }
     return !isApplication;
 }
+#endif
 
 #if BPLATFORM(IOS_FAMILY)
 bool shouldProcessUnconditionallyUseBmalloc()
