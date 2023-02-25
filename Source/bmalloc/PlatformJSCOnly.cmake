8cb1760055c10c700c07cc2c09775e8d3451bc0d
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-02-25 02:38:45 -0800

    Set BUN_FAST_TLS in bmalloc

--- Source/bmalloc/PlatformJSCOnly.cmake
+++ Source/bmalloc/PlatformJSCOnly.cmake
@@ -1,3 +1,8 @@
-if (APPLE)
-    
+if(APPLE)
+endif()
+
+if(BUN_FAST_TLS)
+    add_definitions(-DBUN_FAST_TLS=1)
+    add_definitions(-DUSE_BUN_FAST_TLS=1)
+    add_definitions(-DHAVE_BUN_FAST_TLS=1)
 endif ()
