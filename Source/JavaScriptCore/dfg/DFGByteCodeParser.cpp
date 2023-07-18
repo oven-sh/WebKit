f4633df4aa3a45b8d43ff16949c1a9bfb74be369
dave caruso <me@paperdave.net>
2023-07-18 13:20:16 -0700

    clean

--- Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
+++ Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
@@ -6167,7 +6167,6 @@ void ByteCodeParser::parseBlock(unsigned limit)
 
         case op_wrap_in_async_context_frame: {
             auto bytecode = currentInstruction->as<OpWrapInAsyncContextFrame>();
-            // JSGlobalObject* globalObject = m_graph.globalObjectFor(currentNodeOrigin().semantic);
             set(bytecode.m_dst, addToGraph(WrapInAsyncContextFrame, get(bytecode.m_asyncContext), get(bytecode.m_context)));
             NEXT_OPCODE(op_wrap_in_async_context_frame);
         }
