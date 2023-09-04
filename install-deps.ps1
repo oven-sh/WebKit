623d2437dceff7b419388f8f77e2b61f5e593ef9
CGQAQ <m.jason.liu@outlook.com>
2023-09-04 17:05:32 +0800

    install-deps.ps1

---
+++ install-deps.ps1
@@ -0,0 +1,39 @@
+#Requires -RunAsAdministrator
+
+if ($null -eq (Get-Command "choco" -ErrorAction SilentlyContinue)) 
+{ 
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+if ($null -eq (Get-Command "git" -ErrorAction SilentlyContinue)) 
+{ 
+    choco install -y git
+}
+
+if($null -eq (Get-Command "python" -ErrorAction SilentlyContinue)) 
+{ 
+    choco install -y python3
+}
+
+if($null -eq (Get-Command "perl" -ErrorAction SilentlyContinue)) 
+{ 
+    choco install -y StrawberryPerl
+}
+
+if ($null -eq (Get-Command "ruby" -ErrorAction SilentlyContinue)) 
+{ 
+    choco install -y ruby
+}
+
+if ($null -eq (Get-Command "cmake" -ErrorAction SilentlyContinue)) 
+{ 
+    choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
+}
+
+$VCPKG_ROOT = "$PSScriptRoot\vcpkg"
+$VCPKG_EXE  = "$VCPKG_ROOT\vcpkg.exe"
+if(!(Test-Path -Path $VCPKG_EXE -PathType leaf)) {
+    .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+}
+
+& $VCPKG_EXE install icu --triplet "x64-windows-static"
