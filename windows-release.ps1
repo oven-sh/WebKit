1bda4eb945b894c18bd250a02f394b2d9fa6908a
Yauhen Pahrabniak <emcifuntik@gmail.com>
2024-07-11 20:25:08 +0200

    fix

--- windows-release.ps1
+++ windows-release.ps1
@@ -149,11 +149,19 @@ if (!(Test-Path -Path $ICU_STATIC_ROOT)) {
     Copy-Item -r $ICU_STATIC_ROOT/source/lib/* $ICU_STATIC_LIBRARY/
 
     # JSC expects the static library to be named icudt.lib
-    Copy-Item -r $ICU_STATIC_LIBRARY/sicudt.lib $ICU_STATIC_LIBRARY/icudt.lib
-    Copy-Item -r $ICU_STATIC_LIBRARY/sicutu.lib $ICU_STATIC_LIBRARY/icutu.lib
-    Copy-Item -r $ICU_STATIC_LIBRARY/sicuio.lib $ICU_STATIC_LIBRARY/icuio.lib
-    Copy-Item -r $ICU_STATIC_LIBRARY/sicuin.lib $ICU_STATIC_LIBRARY/icuin.lib
-    Copy-Item -r $ICU_STATIC_LIBRARY/sicuuc.lib $ICU_STATIC_LIBRARY/icuuc.lib
+    if ($CMAKE_BUILD_TYPE -eq "Release") {
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicudt.lib $ICU_STATIC_LIBRARY/icudt.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicutu.lib $ICU_STATIC_LIBRARY/icutu.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuio.lib $ICU_STATIC_LIBRARY/icuio.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuin.lib $ICU_STATIC_LIBRARY/icuin.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuuc.lib $ICU_STATIC_LIBRARY/icuuc.lib
+    } elseif ($CMAKE_BUILD_TYPE -eq "Debug") {
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicudtd.lib $ICU_STATIC_LIBRARY/icudt.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicutud.lib $ICU_STATIC_LIBRARY/icutu.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuiod.lib $ICU_STATIC_LIBRARY/icuio.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuind.lib $ICU_STATIC_LIBRARY/icuin.lib
+        Copy-Item -r $ICU_STATIC_LIBRARY/sicuucd.lib $ICU_STATIC_LIBRARY/icuuc.lib
+    }
 }
 
 Write-Host ":: Configuring WebKit"
