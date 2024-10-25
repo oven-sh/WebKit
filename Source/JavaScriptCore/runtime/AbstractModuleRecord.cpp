9ee1e4d18abe3e94b3b64ed71b19f51b9ae16df5
Jarred Sumner <jarred@jarredsumner.com>
2024-10-24 21:32:06 -0700

    Fix a couple warnings

--- Source/JavaScriptCore/runtime/AbstractModuleRecord.cpp
+++ Source/JavaScriptCore/runtime/AbstractModuleRecord.cpp
@@ -92,6 +92,7 @@ void AbstractModuleRecord::addStarExportEntry(const Identifier& moduleName)
 void AbstractModuleRecord::addImportEntry(const ImportEntry& entry)
 {
     bool isNewEntry = m_importEntries.add(entry.localName.impl(), entry).isNewEntry;
+    UNUSED_PARAM(isNewEntry);
     // This is guaranteed by the parser.
     ASSERT_WITH_MESSAGE(isNewEntry, "Duplicate import entry name '%s'", entry.localName.impl()->utf8().data());
 }
@@ -99,6 +100,7 @@ void AbstractModuleRecord::addImportEntry(const ImportEntry& entry)
 void AbstractModuleRecord::addExportEntry(const ExportEntry& entry)
 {
     bool isNewEntry = m_exportEntries.add(entry.exportName.impl(), entry).isNewEntry;
+    UNUSED_PARAM(isNewEntry);
     // This is guaranteed by the parser.
     ASSERT_WITH_MESSAGE(isNewEntry, "Duplicate export entry name '%s'", entry.exportName.impl()->utf8().data());
 }
