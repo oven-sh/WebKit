054e7e8d76c7e29fdef8f0f6cbe03da2b786a6c8
Dylan Conway <dylan.conway567@gmail.com>
2026-08-27 03:27:48 +0000

    RegisteredSymbolImpl::createPrivate: take the hash from the registry string (already computed by the lookup) instead of rehashing; SymbolImpl's constructors are as upstream again

--- Source/WTF/wtf/text/SymbolImpl.h
+++ Source/WTF/wtf/text/SymbolImpl.h
@@ -72,7 +72,6 @@ public:
 
 protected:
     WTF_EXPORT_PRIVATE static unsigned NODELETE nextHashForSymbol();
-    template<typename CharacterType> static unsigned hashForSymbol(std::span<const CharacterType>, Flags);
 
     friend class StringImpl;
 
