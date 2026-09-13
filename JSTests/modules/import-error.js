65b551e000427be7c9ef8b410c39a6944f670110
Dylan Conway <dylan.conway567@gmail.com>
2026-09-13 06:06:03 +0000

    JSTests: import-error.js without a helper function

--- JSTests/modules/import-error.js
+++ JSTests/modules/import-error.js
@@ -1,20 +1,18 @@
 import { shouldBe } from "./resources/assert.js"
 
-function moduleURL(name) {
-    return import.meta.url.slice(0, import.meta.url.lastIndexOf("/") + 1) + "import-error/" + name;
-}
+const directory = import.meta.url.slice(0, import.meta.url.lastIndexOf("/") + 1);
 
 Promise.all([
     import('./import-error/import-not-found.js')
         .then($vm.abort, function (error) {
-            shouldBe(String(error), `SyntaxError: Export named 'B' not found in module '${moduleURL("export-not-found.js")}'.`);
+            shouldBe(String(error), `SyntaxError: Export named 'B' not found in module '${directory}import-error/export-not-found.js'.`);
         }).catch($vm.abort),
     import('./import-error/import-ambiguous.js')
         .then($vm.abort, function (error) {
-            shouldBe(String(error), `SyntaxError: Export named 'B' cannot be resolved due to ambiguous multiple bindings in module '${moduleURL("export-ambiguous.js")}'.`);
+            shouldBe(String(error), `SyntaxError: Export named 'B' cannot be resolved due to ambiguous multiple bindings in module '${directory}import-error/export-ambiguous.js'.`);
         }).catch($vm.abort),
     import('./import-error/import-default-from-star.js')
         .then($vm.abort, function (error) {
-            shouldBe(String(error), `SyntaxError: Missing 'default' export in module '${moduleURL("export-default-from-star.js")}'.`);
+            shouldBe(String(error), `SyntaxError: Missing 'default' export in module '${directory}import-error/export-default-from-star.js'.`);
         }).catch($vm.abort),
 ]).catch($vm.abort);
