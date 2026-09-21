1142a579aa2b0cd4d5bbc294431e08d89a0594b9
robobun <117481402+robobun@users.noreply.github.com>
2026-09-21 20:07:19 +0000

    [JSC] Parser: isArrowFunctionParameters() parses the parameters with [+Yield] in a generator
    The net change of oven-sh/WebKit#702, head 54004fef9d.

--- Source/JavaScriptCore/parser/Parser.h
+++ Source/JavaScriptCore/parser/Parser.h
@@ -1871,7 +1871,7 @@ private:
     enum class FunctionDefinitionType { Expression, Declaration, Method };
     template <class TreeBuilder> NEVER_INLINE bool parseFunctionInfo(TreeBuilder&, FunctionNameRequirements, bool nameIsInContainingScope, ConstructorKind, SuperBinding, unsigned functionStart, ParserFunctionInfo<TreeBuilder>&, FunctionDefinitionType, std::optional<int> functionConstructorParametersEndPosition = std::nullopt);
     
-    template <class TreeBuilder> ALWAYS_INLINE bool isArrowFunctionParameters(TreeBuilder&);
+    template <class TreeBuilder> ALWAYS_INLINE bool isArrowFunctionParameters(TreeBuilder&, bool& isArrowFunctionWithInvalidParameters);
     
     template <class TreeBuilder, class FunctionInfoType> NEVER_INLINE typename TreeBuilder::FormalParameterList parseFunctionParameters(TreeBuilder&, FunctionInfoType&);
     template <class TreeBuilder> NEVER_INLINE typename TreeBuilder::FormalParameterList createGeneratorParameters(TreeBuilder&, unsigned& parameterCount);
